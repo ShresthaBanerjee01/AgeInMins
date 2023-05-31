8a0a83a2110efd6a44911b9bceeff43e2b0fde5b
Shrestha <shresthabanerjee@gmail.com>
2023-05-31 19:26:02 +0530

    calender started

--- app/src/main/java/com/example/ageinmins/MainActivity.kt
+++ app/src/main/java/com/example/ageinmins/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.ageinmins
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+        btnDatePicker.setOnClickListener{
+            clickDatePicker()
+            Toast.makeText(this, "btnDatePicker pressed", Toast.LENGTH_LONG).show()
+        }
+    }
+    fun clickDatePicker(){
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog()
+        Toast.makeText(this, "btnDatePicker pressed", Toast.LENGTH_LONG).show()
     }
 }
