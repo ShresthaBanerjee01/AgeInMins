eac19e52549466fdd35140e31569fac50d2ae845
Shrestha <shresthabanerjee@gmail.com>
2023-06-01 22:02:07 +0530

    date selection complete

--- app/src/main/java/com/example/ageinmins/MainActivity.kt
+++ app/src/main/java/com/example/ageinmins/MainActivity.kt
@@ -11,12 +11,14 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private var tvSelectedDate : TextView? = null
+    private var tvAgeInMinutes : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
         tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
         btnDatePicker.setOnClickListener{
             clickDatePicker()
         }
@@ -33,6 +35,11 @@ class MainActivity : AppCompatActivity() {
                 tvSelectedDate?.text = selectedDate
                 val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
                 val theDate = sdf.parse(selectedDate)
+                val selectedDateInMinutes = theDate.time /60000
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                val currentDateInMinutes = currentDate.time /60000
+                val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+                tvAgeInMinutes?.text = differenceInMinutes.toString()
             },
             year,
             month,
